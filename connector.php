b7d50a7074848f4d9d569369237c98d70a588bee
mdaffad <13518141@std.stei.itb.ac.id>
2020-10-22 16:29:46 +0700

    db and connection

---
+++ connector.php
@@ -0,0 +1,28 @@
+<?php
+    
+    class Connector {
+        private $server;
+        private $username;
+        private $password;
+        public $connection;
+        function __construct() {
+            $this->server = "willywangky";
+            $this->username = "willywangky";
+            $this->passoword = "willywangky";
+            $this->connection = new mysqli($this->server, $this->username, $this->password);
+            if ($this->connection->connect_error) {
+                die("Connection failed: " . $this->connection->connect_error);
+            }
+            echo "Connected successfully";
+        }
+        function run($query) {
+            if ($this->connection->query($query) === TRUE) {
+                echo "Query success";
+            } else {
+                echo "Error: " . $query . "<br>" . $this->connection->error;
+            }
+        }
+        
+    }
+    
+?>
