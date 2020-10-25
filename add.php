72944ad63f27b6ada0bfb14b79d570ea7ac5a0a0
mdaffad <13518141@std.stei.itb.ac.id>
2020-10-25 16:34:53 +0700

    add.php style

--- add.php
+++ add.php
@@ -12,28 +12,50 @@
         <title>Add Chocolate - Willy-Wanky</title>
         <link rel="stylesheet" type="text/css" href="/assets/styles/common.css">
         <link rel="stylesheet" type="text/css" href="/assets/styles/common-navbar.css">
+        <link rel="stylesheet" type="text/css" href="/assets/styles/DetailUserAdmin.css">
+        
+
     </head>
     <body>
         <?php include 'get-header.php';?>
         <div class="thread">
-            <form action="handle-add.php" method="post" enctype="multipart/form-data">
-                <label for="name">Name</label><br>
-                <input type="text" name="name" required><br>
-                <label for="price">Price</label><br>
-                <input type="number" name="price" required min= "0"><br>
-                <textarea name="description" rows="10" cols="30" required placeholder="Description"></textarea><br>
-                <h2>PHP Image Upload with Size Type Dimension Validation</h2>
-                <div class="form-row">
-                    <div>Choose Image file:</div>
-                    <div>
-                        <input type="file" class="file-input" name="file-input">
+            <div class="opacity-layer clearfix">
+                <div class="main-container clearfix">
+                    <div class="secondary-container clearfix">
+                        <form action="handle-add.php" method="post" enctype="multipart/form-data">
+                            <div class = "content-label">
+                                <label for="name">Name</label><br><br>
+                            </div>
+                            <input class = "name-box" type="text" name="name" required><br><br>
+                            <div class = "content-label">
+                                <label for="price">Price</label><br><br>
+                            </div>
+                            <input class = "amount-box" type="number" name="price" required min= "0" ><br><br>
+                            <div class = "content-label">
+                                <label for="description">Description</label><br><br>
+                            </div>
+                            <textarea class ="description-box" name="description" rows="10" cols="30" required placeholder="Description"></textarea><br><br>
+                            <div class="form-row">
+                                <div class = "content-label">
+                                    <label for="file-input">Choose image</label><br><br>
+                                </div>
+                                <div>
+                                    <input type="file" class="file-input" name="file-input">
+                                </div>
+                            </div>
+                            <br>
+                            <div class = "content-label">
+                                <label for="amount">Amount</label><br><br>
+                            </div>
+                            <input class="amount-box" type="number" name="amount" required min ="0"><br>
+                            <div class ="button-container">
+                            <br>
+                            <input type="submit" name = "upload">
+                            </div>
+                        </form>
                     </div>
                 </div>
-            
-                <label for="amount">Amount</label><br>
-                <input type="number" name="amount" required><br>
-                <input type="submit" name = "upload">
-            </form>
+            </div>
         </div>
         <?php if(!empty($response)) { ?>
             <div class="response <?php echo $response["type"]; ?>
